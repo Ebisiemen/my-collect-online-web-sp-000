206ac4dec561d2c8c3aea6fdc79f0ef7d6872125
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-10 15:14:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   counter = 0 
   new_array = []
   while counter < collection.length 
-  new_array << yield (collection[counter]) 
+  new_array << yield(collection[counter]) 
   counter += 1 
   end 
   new_array
